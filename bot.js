aea62233f2707474c806b1629964e25427a29036
Superduperdavid <kingcoyle@gmail.com>
2016-08-28 13:08:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,7 +146,7 @@ function respond() {
   }
   else if(request.text && botRegexPIT.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://www.youtube.com/channel/UCo22yos1aOqsbBb0YHzCd7A");
+    postMessage("https://www.youtube.com/channel/UCc3bjlY1fF-HKeHXtGpzlnA");
     this.res.end();
   }
   else if(request.text && botRegexSD.test(request.text)) {
