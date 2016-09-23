c2a2c78c5c76f0ef41dd8eb03512e35b0ec7891c
Superduperdavid <kingcoyle@gmail.com>
2016-09-22 23:46:08 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,32 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
-
 var botID = process.env.BOT_ID;
+function sendText(text){
+  UrlFetchApp.fetch("https://api.groupme.com/v3/bots/post", {"method":"post", "payload":'{"bot_id":"' + botId + '","text":"' + text + '"}'})
+}
+
+function doPost(e){
+  var post = JSON.parse(e.postData.getDataAsString());
+  var text = post.text;
+  var user_id = post.user_id; // Used to tell if its system sending the message (returns 0)
+
+// Rejoined a group
+  if (text.indexOf("has rejoined the") > -1 && user_id == 0){
+     sendText("Welcome back");
+
+// Joined a group
+  }else if(text.indexOf("has joined the") > -1 && user_id == 0){
+     sendText("Welcome to Hell!");
+
+// Left a group
+  }else if(text.indexOf("has left the") > -1 && user_id == 0){
+     sendText("Cya sucker");
+
+// Changed NickName
+  }else if(text.indexOf("changed name to") > -1 && user_id == 0){
+     sendText("I liked the old one better");
+  }
+}
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
